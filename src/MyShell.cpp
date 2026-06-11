2247e3bc8e93cc5f41e2dd63ad276644e3dbf3d0
Soyorin-suki <2374237963@qq.com>
2026-06-11 17:22:20 +0800

    feat: 完成了基本的外部命令调用，完善了部分文档

--- src/MyShell.cpp
+++ src/MyShell.cpp
@@ -2,6 +2,8 @@
 #include <vector>
 #include <string>
 #include <iostream>
+#include <unistd.h>
+#include <sys/wait.h>
 using std::string;
 using std::vector;
 
@@ -56,8 +58,28 @@ int MyShell::execute(const vector<string>&args){
 	int status=1;
 	if(args.empty())return status;
 	pid_t pid,wpid;
-	
-	
-	return status;
+	vector<char*>argv;
+	for(auto&arg:args){
+		argv.push_back(const_cast<char*>(arg.c_str()));
+	}
+	argv.push_back(nullptr);
+	pid=fork();
+	if(pid<0){
+		// 启动失败
+		perror("MyShell启动失败");
+	}else if(pid==0){
+		// 子进程
+		if(execvp(argv[0],argv.data())==-1){
+			perror("MyShell");
+		}
+		exit(EXIT_FAILURE);
+	}else{
+		do{
+			wpid=waitpid(pid,&status,WUNTRACED);
+		}while(!WIFEXITED(status)&&!WIFSIGNALED(status));
+	}
+
+
+	return 1;
 }
 
